339e5472d182c7f594e80aec5f7cc9bb1118fc02
AlexWufan <mengnanszw@gmail.com>
2016-11-04 23:13:27 -0400

    delete dota today

---
+++ 167. Two Sum II - Input array is sorted.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # dic = {}
+        # for i in range(len(numbers)):
+        # 	if target-numbers[i] in dic:
+        # 		return [dic[target-numbers[i]]+1, i+1]
+        # 	else:
+        # 		dic[numbers[i]] = i 
+        
+
+        # two pointer #############
+        l = 0
+        r = len(numbers)-1
+        while l<r:
+        	if numbers[l]+numbers[r] == target:
+        		return [l+1, r+1]
+        	elif numbers[l]+numbers[r] > target:
+        		r -= 1
+        	else:
+        		l += 1
+
+if __name__=='__main__':
+	aSolution = Solution()
+	print(aSolution.twoSum([2,7,11,15],9))
+
