9d3f078c911504563668cde188d2c2c07260c08e
AlexWufan <mengnanszw@gmail.com>
2016-11-05 23:41:14 -0400

    60

--- 419. Battleships in a Board.py
+++ 419. Battleships in a Board.py
@@ -4,8 +4,4 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: int
         """
-        count = 0
-        for x in board:
-        	if x.count('X') > count:
-        		count = x.count('X')
-        return count
+        # tbc
