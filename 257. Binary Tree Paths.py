93acd9a8ad2b701b9e974936e77a7c2e92945836
AlexWufan <mengnanszw@gmail.com>
2016-11-07 21:43:19 -0500

    tree2

--- 257. Binary Tree Paths.py
+++ 257. Binary Tree Paths.py
@@ -5,34 +5,61 @@
 #         self.left = None
 #         self.right = None
 
+# DFS
 class Solution:
     # @param {TreeNode} root
     # @return {string[]}
     def binaryTreePaths(self, root):
     	if not root:
     		return []
-    	res = [], path = [root.val], stack = [root]
+    	res, stack =[], [[root, str(root.val)]]
     	while stack:
-    		node = stack.pop()
+    		node, path = stack.pop()
     		if not node.left and not node.right:
     			res.append(path)
-    			path.pop()
-    		if node.left:
-    			stack.append(node.left)
-    			path.append(node.left.val)
     		if node.right:
-    			stack.append(node.right)
-    			path.append(node.right.val)
+    			stack.append([node.right, path + '->' + str(node.right.val)])
+    		if node.left:
+    			stack.append([node.left, path + '->' + str(node.left.val)])
     	# modify res to string 
     	return res
 
+# BFS
+class Solution:
+    # @param {TreeNode} root
+    # @return {string[]}
+    def binaryTreePaths(self, root):
+    	if not root:
+    		return []
+    	res, queue =[], [[root, str(root.val)]]
+    	while queue:
+    		node, path = queue.pop(0)
+    		if not node.left and not node.right:
+    			res.append(path)
+    		if node.right:
+    			queue.append([node.right, path + '->' + str(node.right.val)])
+    		if node.left:
+    			queue.append([node.left, path + '->' + str(node.left.val)])
+    	return res
 
+# recursive
+class Solution:
+    # @param {TreeNode} root
+    # @return {string[]}
+    def binaryTreePaths(self, root):
+    	if not root:
+    		return []
+    	res = []
+    	self.DFS(root,str(root.val),res)
+    	return res
 
-
-
-
-
-
+    def DFS(self,node,path,res):
+    	if not node.left and not node.right:
+    		res.append(path)
+    	if node.right:
+    		self.DFS(node.right, path + '->' + str(node.right.val), res)
+    	if node.left:
+    		self.DFS(node.left, path + '->' + str(node.left.val), res)
 
 
 
