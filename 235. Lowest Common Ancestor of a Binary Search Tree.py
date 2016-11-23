3620603984748252e41ea11cf971717dec21e98f
AlexWufan <mengnanszw@gmail.com>
2016-11-22 23:22:12 -0500

    11.22

--- 235. Lowest Common Ancestor of a Binary Search Tree.py
+++ 235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -13,4 +13,50 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
-        
+        #还是不知道为什么错，
+        '''
+        if not root : return None
+        path = [root]
+        stack = [[root, path]]
+        while stack:
+        	node, path = stack.pop()
+        	if node == p:
+        		path1 = path
+        	elif node == q:
+        		path2 = path
+        	if node.left:
+        		stack.append([node.left, path.append(node.left)])
+    		if node.right:
+    			stack.append([node.right, path.append(node.right)])
+    	for i in range(len(path1)):
+    		if path1[i] == path2[i]:
+    			continue
+    		elif path[i] != path2[i]:
+    			return path1[i-1]
+    		else:
+    			return path2[i+1]
+    	'''
+
+    	if not root: return None
+    	if root.val > p.val and root.val > q.val:
+    		return lowestCommonAncestor(root.left, p, q)
+    	if root.val < p.val and root.val < q.val:
+    		return lowestCommonAncestor(root.right, p, q)
+    	else: return root
+
+
+
+
+
+#stefan Solution
+	def lowestCommonAncestor(self, root, p, q):
+	    while (root.val - p.val) * (root.val - q.val) > 0:
+	        root = (root.left, root.right)[p.val > root.val]
+	    return root
+
+	def lowestCommonAncestor(self, root, p, q):
+    if p.val < root.val > q.val:
+        return self.lowestCommonAncestor(root.left, p, q)
+    if p.val > root.val < q.val:
+        return self.lowestCommonAncestor(root.right, p, q)
+    return root
--- 463. Island Perimeter.py
+++ 463. Island Perimeter.py
@@ -5,14 +5,15 @@ class Solution(object):
         :rtype: int
         """
         n = len(grid)
+        m = len(grid[0])
         res = 0
         for i in range(n):
-            for j in range(n):
+            for j in range(m):
                 if grid[i][j] == 1:
                     res+=4
                     if j-1>=0 and grid[i][j-1]:
                         res-=1
-                    if j+1<n and grid[i][j+1]:
+                    if j+1<m and grid[i][j+1]:
                         res-=1
                     if i-1>=0 and grid[i-1][j]:
                         res-=1
