6e20900c1b20402103efcd04366460862d12aa3f
AlexWufan <mengnanszw@gmail.com>
2016-11-24 16:41:38 -0500

    Tree traverse

---
+++ 144. Binary Tree Preorder Traversal.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+#recursive solution
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        self.helper(root, res)
+        return res
+    def helper(self, root, res):
+    	if root:
+    		res.append(root.val)
+    		self.helper(root.left, res)
+    		self.helper(root.right, res)
+
+
+
+
+# iteratively
+
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root: return []
+        res, stack = [], [root]
+        while stack:
+        	node = stack.pop()
+        	res.append(node.val)
+        	if node.right:
+        		stack.append(node.right)
+        	if node.left:
+        		stack.append(node.left)
+        	# or
+        	# if node:
+        	# 	stack.append(node.right)
+        	# 	stack.append(node.left)
+        	# 	res.append(node.val)
+        return res
---
+++ 94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,64 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        self.helper(root, res)
+        return res
+
+    def helper():
+    	if root:
+    		self.helper(root, res)
+    		res.append(root.val)
+    		self.helper(root, res)
+
+
+
+
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        stack = [root]
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return res
+            node = stack.pop()
+            res.append(node.val)
+            root = node.right
+
+
+#better 
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        stack = []
+        while stack or root:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                node = stack.pop()
+                res.append(node.val)
+                root = node.right
+        return res
